23fd8cbaf971b2e0c6f58473f392e8b31ba262d0
razeenmahroof <81762966+razeenmahroof@users.noreply.github.com>
2025-01-02 14:09:20 +0530

    Add files via upload

---
+++ reg-keycloak/src/main/java/com/example/reg_keycloak/RegKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.example.reg_keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegKeycloakApplication.class, args);
+	}
+
+}
---
+++ reg-keycloak/src/test/java/com/example/reg_keycloak/RegKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reg_keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
